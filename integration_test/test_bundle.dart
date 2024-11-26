fd3b496b40e18617ff886cf4197594eb81fe1850
abi <abi.balogun@gohenry.co.uk>
2024-11-26 15:22:06 +0000

    Updated iosyaml file

--- integration_test/test_bundle.dart
+++ integration_test/test_bundle.dart
@@ -9,7 +9,7 @@ import 'package:patrol/src/native/contracts/contracts.dart';
 import 'package:test_api/src/backend/invoker.dart';
 
 // START: GENERATED TEST IMPORTS
-import 'fail_login_test.dart' as fail_login_test;
+import 'e2e_test.dart' as e2e_test;
 // END: GENERATED TEST IMPORTS
 
 Future<void> main() async {
@@ -69,7 +69,7 @@ Future<void> main() async {
   });
 
   // START: GENERATED TEST GROUPS
-  group('fail_login_test', fail_login_test.main);
+  group('e2e_test', e2e_test.main);
   // END: GENERATED TEST GROUPS
 
   final dartTestGroup = await testExplorationCompleter.future;
